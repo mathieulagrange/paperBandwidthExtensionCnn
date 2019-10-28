3f03820bbcdb333f96742a566c810623be1b2e98
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2019-10-28 16:00:18 +0100

    deploy demo page

--- demo/comments.js
+++ demo/comments.js
@@ -2,9 +2,9 @@
 // The number of comments do not need to be same as the number of tables
 // Please use plain text with \n for carriage return
 
-var report = "Please describe your experiment \n  by editing the comment.js file";
+var report = "Welcome to the companion page of the paper entitled  \n Bandwidth extension of musical audio signals with no side information \n using dilated convolutional neural networks   \n feel free to contact us \n mathieu dot lagrange at cnrs dot fr \n  github repo : \n https://github.com/mathieulagrange/paperBandwidthExtension";
 
 var comments = [
-    "Please insert a comment for this table \n by editing the comment.js file", 
+    "",
     ""
 ];
