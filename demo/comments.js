dd1fad035c8d8293f6cafe5c16338e8146897a24
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2020-02-10 14:14:44 +0100

    provide guidance for plyaing audio

--- demo/comments.js
+++ demo/comments.js
@@ -2,7 +2,7 @@
 // The number of comments do not need to be same as the number of tables
 // Please use plain text with \n for carriage return
 
-var report = "Welcome to the companion page of the paper entitled  \n Bandwidth extension of musical audio signals with no side information \n using dilated convolutional neural networks   \n feel free to contact us \n mathieu dot lagrange at cnrs dot fr \n  github repo : \n https://github.com/mathieulagrange/paperBandwidthExtension";
+var report = "Welcome to the companion page of the paper entitled  \n Bandwidth extension of musical audio signals with no side information \n using dilated convolutional neural networks   \n feel free to contact us \n mathieu dot lagrange at cnrs dot fr \n  github repo : \n https://github.com/mathieulagrange/paperBandwidthExtension \n Instructions for the audio player: \n The result of each method can be listened to by selecting them using the playing symbol on the rightmost column. The bottom player can used to have global playing control.";
 
 var comments = [
     "",
