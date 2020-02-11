dd41d7b574818d0b17ec5897945548005c254d35
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2020-02-11 14:59:31 +0100

    demo comment

--- demo/comments.js
+++ demo/comments.js
@@ -2,7 +2,7 @@
 // The number of comments do not need to be same as the number of tables
 // Please use plain text with \n for carriage return
 
-var report = "Github repo : https://github.com/mathieulagrange/paperBandwidthExtension \n Instructions for the audio player: \n The result of each method can be listened to by selecting them using the playing symbol on the rightmost column. The bottom player can used to have global playing control.";
+var report = "Github repo : https://github.com/mathieulagrange/paperBandwidthExtension \n Instructions for the audio player: \n Each method can be listened to by selecting it using the playing symbol on the rightmost column. \n The bottom player can used to have global playing control.";
 
 var comments = [
     "",
